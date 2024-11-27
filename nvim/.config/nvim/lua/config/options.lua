f403a32c2e62f9e71e43f6ec3b50329c7453322a
John Fujitani <jfuji6@gmail.com>
2024-11-26 18:51:44 -0800

    nvim

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -2,15 +2,20 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.scrolloff = 20
+vim.opt.tabstop = 4 -- A TAB character looks like 4 spaces
+vim.opt.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
+vim.opt.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
+vim.opt.shiftwidth = 4 -- Number of spaces inserted when indenting
+
 return {
-  {
-    "nvim-lualine/lualine.nvim",
-    sections = {
-      lualine_z = {
-        function()
-          return " " .. os.date("%I:%M")
-        end,
-      },
+    {
+        "nvim-lualine/lualine.nvim",
+        sections = {
+            lualine_z = {
+                function()
+                    return " " .. os.date("%I:%M")
+                end,
+            },
+        },
     },
-  },
 }
--- nvim/.config/nvim/lua/plugins/disable.lua
+++ nvim/.config/nvim/lua/plugins/disable.lua
@@ -1,5 +1,5 @@
 return {
-  { "echasnovski/mini.surround", enabled = false },
+  { "echasnovski/mini.surround", enabled = true },
   { "echasnovski/mini.pairs", enabled = true },
   { "folke/flash.nvim", enabled = false },
   { "garymjr/nvim-snippets", enabled = false },
