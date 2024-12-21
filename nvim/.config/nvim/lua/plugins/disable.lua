0a12c1c35b745d4d24b0f6957ebece78ede1cc28
John Fujitani <jfuji6@gmail.com>
2024-12-21 14:55:36 -0800

    update lazy.nvim

--- nvim/.config/nvim/lua/plugins/disable.lua
+++ nvim/.config/nvim/lua/plugins/disable.lua
@@ -1,6 +1,3 @@
 return {
-  { "echasnovski/mini.surround", enabled = true },
-  { "echasnovski/mini.pairs", enabled = true },
   { "folke/flash.nvim", enabled = false },
-  { "garymjr/nvim-snippets", enabled = false },
 }
