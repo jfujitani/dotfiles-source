6f3dd3db138dae71ab9647c908791b00d19a14b6
John Fujitani <jfuji6@gmail.com>
2025-03-13 22:37:53 -0700

    nvim configf

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -6,16 +6,16 @@ vim.opt.tabstop = 4 -- A TAB character looks like 4 spaces
 vim.opt.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
 vim.opt.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
 vim.opt.shiftwidth = 4 -- Number of spaces inserted when indenting
-
+vim.g.autoformat = false
 return {
-    {
-        "nvim-lualine/lualine.nvim",
-        sections = {
-            lualine_z = {
-                function()
-                    return " " .. os.date("%I:%M")
-                end,
-            },
-        },
+  {
+    "nvim-lualine/lualine.nvim",
+    sections = {
+      lualine_z = {
+        function()
+          return " " .. os.date("%I:%M")
+        end,
+      },
     },
+  },
 }
