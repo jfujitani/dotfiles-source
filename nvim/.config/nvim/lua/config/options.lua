583416b1642fe611b8b8044a72b884387929a7d4
John Fujitani <jfuji6@gmail.com>
2025-03-23 17:12:51 -0700

    formatting

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -2,11 +2,11 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.scrolloff = 20
-vim.opt.tabstop = 4 -- A TAB character looks like 4 spaces
+vim.opt.tabstop = 2 -- A TAB character looks like 4 spaces
 vim.opt.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
-vim.opt.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
-vim.opt.shiftwidth = 4 -- Number of spaces inserted when indenting
-vim.g.autoformat = false
+vim.opt.softtabstop = 2 -- Number of spaces inserted instead of a TAB character
+vim.opt.shiftwidth = 2 -- Number of spaces inserted when indenting
+vim.g.autoformat = true
 vim.g.lazyvim_picker = "telescope"
 return {
   {
