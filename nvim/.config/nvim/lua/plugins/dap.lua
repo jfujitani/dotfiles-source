05e8259117b282f22a184e06e8b43ff90ec788b0
John Fujitani <jfuji6@gmail.com>
2025-03-24 23:25:29 -0700

    break pooint toggle f9

--- nvim/.config/nvim/lua/plugins/dap.lua
+++ nvim/.config/nvim/lua/plugins/dap.lua
@@ -2,6 +2,7 @@ return {
   {
     "mfussenegger/nvim-dap",
     keys = {
+        { "<f9>", function() require("dap").toggle_breakpoint() end, desc = "continue", },
         { "<f5>", function() require("dap").continue() end, desc = "continue", },
         { "<C-f5>", function() require("dap").terminate() end, desc = "terminate", },
         { "<f10>", function() require("dap").step_over() end, desc = "step over", },
