64de305badef2863e6fd476e2bfb962b492bdc9d
John Fujitani <jfuji6@gmail.com>
2025-04-26 13:54:02 -0700

    dap keybinding, surround

--- nvim/.config/nvim/lua/plugins/dap.lua
+++ nvim/.config/nvim/lua/plugins/dap.lua
@@ -4,7 +4,7 @@ return {
     keys = {
         { "<f9>", function() require("dap").toggle_breakpoint() end, desc = "continue", },
         { "<f5>", function() require("dap").continue() end, desc = "continue", },
-        { "<C-f5>", function() require("dap").terminate() end, desc = "terminate", },
+        { "<S-f5>", function() require("dap").terminate() end, desc = "terminate", },
         { "<f10>", function() require("dap").step_over() end, desc = "step over", },
         { "<f11>", function() require("dap").step_into() end, desc = "step into", },
         { "<f12>", function() require("dap").step_out() end, desc = "step out", },
