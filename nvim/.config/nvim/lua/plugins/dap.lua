717ed5e74eaa4f96774203891a757c6f192432c3
John Fujitani <jfuji6@gmail.com>
2025-05-25 21:10:39 -0700

    config formats

--- nvim/.config/nvim/lua/plugins/dap.lua
+++ nvim/.config/nvim/lua/plugins/dap.lua
@@ -2,12 +2,12 @@ return {
   {
     "mfussenegger/nvim-dap",
     keys = {
-        { "<f9>", function() require("dap").toggle_breakpoint() end, desc = "continue", },
-        { "<f5>", function() require("dap").continue() end, desc = "continue", },
-        { "<S-f5>", function() require("dap").terminate() end, desc = "terminate", },
-        { "<f10>", function() require("dap").step_over() end, desc = "step over", },
-        { "<f11>", function() require("dap").step_into() end, desc = "step into", },
-        { "<f12>", function() require("dap").step_out() end, desc = "step out", },
+      { "<f9>", function() require("dap").toggle_breakpoint() end, desc = "continue", },
+      { "<f5>", function() require("dap").continue() end, desc = "continue", },
+      { "<S-f5>", function() require("dap").terminate() end, desc = "terminate", },
+      { "<f10>", function() require("dap").step_over() end, desc = "step over", },
+      { "<f11>", function() require("dap").step_into() end, desc = "step into", },
+      { "<f12>", function() require("dap").step_out() end, desc = "step out", },
     },
-  },
+  }
 }
