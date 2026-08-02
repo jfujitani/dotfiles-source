4098d2c2d6dc61769f30a8a0f18ad9dafa4960c6
John Fujitani <jfuji6@gmail.com>
2026-08-01 22:20:30 -0700

    herdr vim navigation

--- nvim/.config/nvim/lua/plugins/tmux-navigator.lua
+++ nvim/.config/nvim/lua/plugins/tmux-navigator.lua
@@ -5,13 +5,18 @@ return {
     "TmuxNavigateDown",
     "TmuxNavigateUp",
     "TmuxNavigateRight",
-    "TmuxNavigatePrevious",
   },
   keys = {
     { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
     { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
     { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
     { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
-    { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
   },
+  lazy = false,
+  init = function()
+    vim.g.tmux_navigator_no_mappings = 1
+  end,
+  config = function()
+    dofile(vim.fn.expand("/home/jfujitani/.config/herdr/plugins/github//vim-herdr-navigation-a8bf42123d81/editor/nvim.lua"))
+  end,
 }
